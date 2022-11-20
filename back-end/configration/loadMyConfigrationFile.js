b97b285891c3443eccea9616e6a1d5e7a0a1ed9d
Arbaz khan <arbaz582021@gmail.com>
2022-11-20 10:27:53 +0500

    add

--- back-end/configration/loadMyConfigrationFile.js
+++ back-end/configration/loadMyConfigrationFile.js
@@ -2,15 +2,15 @@ const dotenv = require('dotenv');
 
 let MyEnviorment={};
 if(process.env.NODE_ENV === 'testing'){
-    MyEnviorment = dotenv.config({path:`${__dirname}/../Application-Configuration-Test.env`});
+    MyEnviorment = dotenv.config({path:`${__dirname}/../Application_confirgration-Test.env`});
 }
 
 if(process.env.NODE_ENV === 'development'){
-     MyEnviorment = dotenv.config({path:`${__dirname}/../Application-Configuration-Dev.env`});
+     MyEnviorment = dotenv.config({path:`${__dirname}/../Application_confirgration_dev.env`});
 }
 
 if(process.env.NODE_ENV === 'production'){
-     MyEnviorment = dotenv.config({path:`${__dirname}/../Application-Configuration-Prod.env`})
+     MyEnviorment = dotenv.config({path:`${__dirname}/../Application_confirgration_Prod.env`})
 
 }
 console.log(MyEnviorment)
