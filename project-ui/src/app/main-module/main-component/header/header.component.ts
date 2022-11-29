aa80174b3a675fae11d1227e3213effbbc59a54c
Arbaz khan <arbaz582021@gmail.com>
2022-11-30 00:11:51 +0500

    add

--- project-ui/src/app/main-module/main-component/header/header.component.ts
+++ project-ui/src/app/main-module/main-component/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  @ViewChild(MatSidenav)
+ sidenav!: MatSidenav;
+  constructor(private observer: BreakpointObserver) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res:any) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
   }
 
 }
+function ngAfterViewInit() {
+  throw new Error('Function not implemented.');
+}
+
