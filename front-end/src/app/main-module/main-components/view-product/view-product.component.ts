5da4680dffb8333a1fd1c964fa9be4e7d6e09d20
Arbaz <arbaz582021@gmail.com>
2022-12-27 18:55:45 +0500

    add

--- front-end/src/app/main-module/main-components/view-product/view-product.component.ts
+++ front-end/src/app/main-module/main-components/view-product/view-product.component.ts
@@ -13,39 +13,39 @@ import { ProductService } from 'src/app/shared/services/product.service';
 })
 
 export class ViewProductComponent implements OnInit {
-  public url : string | any ='http://localhost:8686/';
-public productObject:ProductInterface | any={}
-public selectedSizes:any=[];
-public selectedQuantity : any =0;
-public totalQuantity:any
-public enumButtonText = BUTTONTEXT;
+  public url: string | any = 'http://localhost:8686/';
+  public productObject: ProductInterface | any = {}
+  public selectedSizes: any = [];
+  public selectedQuantity: any = 0;
+  public totalQuantity: any
+  public enumButtonText = BUTTONTEXT;
 
-public offCanvas='';
-// 
+  public offCanvas = '';
+  // 
   constructor(
-    private activatedRoute:ActivatedRoute,
-    private productService:ProductService,
-    private LocalStoragesmanagementService:LocalStoragesmanagementServiceService,
-    private toastrService:ToastrService
-    
-    ) { 
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService,
+    private LocalStoragesmanagementService: LocalStoragesmanagementServiceService,
+    private toastrService: ToastrService
+
+  ) {
     this.getValueFromParms()
   }
 
   ngOnInit(): void {
   }
 
-  getValueFromParms(){
-    const parmsId=this.activatedRoute.snapshot.paramMap.get('id')
+  getValueFromParms() {
+    const parmsId = this.activatedRoute.snapshot.paramMap.get('id')
     console.log(parmsId)
-    this.productService.GetAllDataById(parmsId).subscribe((res:any)=>{
-      this.productObject=res.Result;
-      this.totalQuantity=this.productObject.quantity;
-     
+    this.productService.GetAllDataById(parmsId).subscribe((res: any) => {
+      this.productObject = res.Result;
+      this.totalQuantity = this.productObject.quantity;
+
 
     })
   }
-  getSizes(event:any){
+  getSizes(event: any) {
     if (event.target.checked) {
       this.selectedSizes.push(event.target.value);
     }
@@ -55,22 +55,52 @@ public offCanvas='';
     }
     console.log(this.selectedSizes)
   }
-   public derease(){
-  if(this.selectedQuantity>0){
-    this.selectedQuantity--;
-  }
-  }
-   public increase(){
-  if(this.selectedQuantity < this.totalQuantity){
-   this.selectedQuantity++;
-  }
-  }
-  addToCart(){
-    if(this.selectedSizes.length <= 0){
-       this.toastrService.error("please select the sizes")
-    }
-    else{
-      this.offCanvas='offcanvas'
+  public derease() {
+    if (this.selectedQuantity > 0) {
+      let getDataFromLocalStorage = this.LocalStoragesmanagementService.getCartFromLocalStorage();
+      if (getDataFromLocalStorage !== null) {
+        getDataFromLocalStorage.selectedQuantity--;
+        this.LocalStoragesmanagementService.addToCart(getDataFromLocalStorage);
+      }
+      this.selectedQuantity--;
     }
   }
+  public increase() {
+    if (this.selectedQuantity < this.totalQuantity) {
+      this.selectedQuantity++;
+    }
+  }
+  addToCart() {
+    if (this.selectedSizes.length <= 0) {
+      this.toastrService.error("please select the sizes")
+    }
+    else {
+      this.offCanvas = 'offcanvas'
+      let { _id, category, color, companyName, description, price, productName, quantity } = this.productObject;
+      let processedCartObject = {
+        productId: _id,
+        imageUrl: this.productObject?.ProductImages[0]?.ProductImageUrl,
+        selectedSizes: this.selectedSizes,
+        selectedQuantity: this.selectedQuantity,
+        category, color, companyName, description, price, productName, quantity
+      }
+
+      let processedCartArray = [];
+      if (this.LocalStoragesmanagementService.getCartFromLocalStorage() !== null) {
+        let valueAlreadyExists = this.LocalStoragesmanagementService.getCartFromLocalStorage();
+        valueAlreadyExists.forEach((element: any) => {
+          processedCartArray.push(element);
+        });
+      }
+
+    }
+
+  }
+
 }
+
+
+
+
+
+
